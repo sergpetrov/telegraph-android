f39dca9dc7c273d75d9d955668260c2475cd4c7b
sergpetrov <s.a.petrov.spb@gmail.com>
2020-03-01 16:55:22 +0300

    * fix server config

--- app/src/main/java/com/telex/utils/TimberTrees.kt
+++ app/src/main/java/com/telex/utils/TimberTrees.kt
@@ -3,6 +3,8 @@ package com.telex.utils
 import android.util.Log
 import com.bumptech.glide.load.engine.GlideException
 import com.crashlytics.android.Crashlytics
+import com.telex.base.exceptions.NoNetworkConnectionException
+import com.telex.base.exceptions.ProxyConnectionException
 import java.io.IOException
 import timber.log.Timber
 
@@ -17,10 +19,18 @@ class ReleaseTree : Timber.Tree() {
             if (throwable !is GlideException || throwable.rootCauses.find { it is IOException } == null) {
                 if (throwable == null) {
                     Crashlytics.logException(UnknownError(message))
-                } else {
+                } else if (!IGNORED_ERRORS.contains(throwable::class.java)) {
                     Crashlytics.logException(throwable)
                 }
             }
         }
+    }
+
+    companion object {
+        private val IGNORED_ERRORS =
+                arrayListOf(
+                        ProxyConnectionException::class.java,
+                        NoNetworkConnectionException::class.java
+                )
     }
 }
--- base/src/main/java/com/telex/base/model/source/remote/interceptor/GlideInterceptor.kt
+++ base/src/main/java/com/telex/base/model/source/remote/interceptor/GlideInterceptor.kt
@@ -10,23 +10,15 @@ import okhttp3.Response
  */
 class GlideInterceptor(private val serverManager: ServerManager) : Interceptor {
 
-    private val wrongTelegraphServer = "a-telegraph.stel.com"
-
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
-        if ((request.url().host() == ServerConfig.Telegraph.server || request.url().host() == wrongTelegraphServer) &&
+        if ((request.url().host() == ServerConfig.Telegraph.server) &&
                 !serverManager.isUserProxyServerEnabled() &&
                 serverManager.getCurrentServerConfig() != ServerConfig.Telegraph
         ) {
             request = request
                     .newBuilder()
-                    .apply {
-                        if (request.url().toString().contains(wrongTelegraphServer)) {
-                            url(request.url().toString().replace(wrongTelegraphServer, serverManager.getCurrentServerConfig().server))
-                        } else {
-                            url(request.url().toString().replace(ServerConfig.Telegraph.server, serverManager.getCurrentServerConfig().server))
-                        }
-                    }
+                    .url(request.url().toString().replace(ServerConfig.Telegraph.server, serverManager.getCurrentServerConfig().server))
                     .build()
         }
         return chain.proceed(request)
