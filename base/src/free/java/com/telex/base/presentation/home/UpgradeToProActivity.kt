2360edad673406544a26f2f1c8647a72e433313f
sergpetrov <s.a.petrov.spb@gmail.com>
2020-09-20 23:04:00 +0300

    * fix system nav bar style

--- base/src/free/java/com/telex/base/presentation/home/UpgradeToProActivity.kt
+++ base/src/free/java/com/telex/base/presentation/home/UpgradeToProActivity.kt
@@ -2,9 +2,11 @@ package com.telex.base.presentation.home
 
 import android.os.Bundle
 import com.telex.base.R
+import com.telex.base.extention.applySystemWindowInsetsPadding
 import com.telex.base.presentation.base.BaseActivity
 import com.telex.base.utils.ViewUtils
 import kotlinx.android.synthetic.free.activity_upgrade_to_pro.*
+import kotlinx.android.synthetic.free.activity_upgrade_to_pro.closeImageView
 
 /**
  * @author Sergey Petrov
@@ -17,8 +19,8 @@ class UpgradeToProActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         setupStatusBar()
+        rootLayout.applySystemWindowInsetsPadding(applyTop = true, applyBottom = true)
         closeImageView.setOnClickListener { finish() }
-
         installProButton.setOnClickListener {
             ViewUtils.openPro(this)
             finish()
